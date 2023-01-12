d382b422c743ee33aa59ba36bb7233946a43d71b
Rachel Brendel <brendelrachel@gmail.com>
2023-01-11 20:50:29 -0600

    Add places array and a pop method

--- friends.js
+++ friends.js
@@ -2,3 +2,6 @@ var arrays = ["Samee", "Wade", "Matt", "Liz", "Pie"];
 console.log(arrays[3]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+var places = ["Kentucky", "Vermont", "Ohio", "Virginia", "Texas"];
+places.pop();
+console.log(places);
